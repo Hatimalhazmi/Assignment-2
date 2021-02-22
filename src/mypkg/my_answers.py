4ab2965753e277fde5bf3b4e9535650ed07536ae
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-02-22 14:13:30 -0600

    Update my_answers.py

--- src/mypkg/my_answers.py
+++ src/mypkg/my_answers.py
@@ -12,25 +12,25 @@ def tuple():
     # Assign the first element of the tuple "t1" to x
     t1 = (11, 100, 99, 1000, 999)
 
-    x = t1[0]
+    x =
 
     # Print the second from the last element of the tuple "t1".
-    y = t1[-2]
+    y = 
 
     
     #What's the index of 2 in the tuple "t2"?
     t2 = (55, 777, 54, 6, 76, 101, 1, 2, 8679, 99, 123, 99, 76)
-    z = t2.index(2)
+    z = 
 
     #How many times does 99 occur in the tuple "t2"?
-    f =  t2.count(99)
+    f =  
 
     # What is the sum of all the numbers in the tuple "t2"?
     
-    m = sum(t2)
+    m = 
 
     # What is the maximum value in the tuple "t2"?
-    n = max(t2)
+    n =
 
     return x, y, z, f, m, n
 
